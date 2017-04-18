ba76cecb5e0b7dae46dc1bdee40aa2a21f6527dd
Love4653 <league@iMac.attlocal.net>
2017-04-17 18:25:00 -0700

    h
    jh

--- src/level1/myGUI.java
+++ src/level1/myGUI.java
@@ -1,18 +1,38 @@
 package level1;
 
+import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 
 public class myGUI {
 
+	JButton button;
+	JButton button2;
 	JFrame frame;
 	JPanel panel;
 	void setup(){
 	frame = new JFrame();
 	frame.setVisible(true);
-	frame.setSize(500, 500);
+	panel = new JPanel();
 	frame.add(panel);
+	button = new JButton("I am a button");
+	button2 = new JButton("Click me");
+	panel.add(button);
+	button2 = new JButton("Click me");
+	button.addActionListener(
+			(e)->{
+				panel.add(button2);
+				panel.repaint();
+				JOptionPane.showMessageDialog(null, "hi");
+				frame.setSize(500, 500);
+			}
+			
+			
+			
+			);
 	
+	frame.setSize(500, 500);
 	
 	
 }
