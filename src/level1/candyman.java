0c9c115230b1e8a1cc0f41cc1ac6dfb2a54dfdc9
Dexter H <league@WorthyMini-6.attlocal.net>
2017-09-25 18:30:09 -0700

    candyyy
    candy

--- src/level1/candyman.java
+++ src/level1/candyman.java
@@ -1,5 +1,42 @@
 package level1;
 
-public class candyman {
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+public class candyman implements ActionListener {
+	int timesPressed = 0;
+public candyman() {
+	this.timesPressed = timesPressed;
+	JFrame frame = new JFrame();
+	JPanel panel = new JPanel();
+	JButton button = new JButton("CANDYYYYYYYYYYYYY");
+	frame.add(panel);
+	panel.add(button);
+	frame.setVisible(true);
+	frame.pack();
+button.addActionListener(this);
 }
+public static void main(String[] args) {
+candyman candyman = new candyman();	
+
+			
+}
+
+			
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				// TODO Auto-generated method stub
+				if(timesPressed >= 4) {
+					System.out.println("button pressed 5 times");
+					
+				}else {
+					timesPressed++;
+					System.out.println(timesPressed);
+				}
+			}
+			}
+
