54269e6493ecc0ed7cfd1f9b48f9bd2411181cbc
munchman <nootnoot@gmail.com>
2018-10-15 18:23:17 -0700

    Rectangle

---
+++ src/level1/rectangle.java
@@ -0,0 +1,19 @@
+package level1;
+
+public class rectangle {
+private int width;
+private int height;
+	public rectangle() {
+		width = 7;
+		height = 8;
+	}
+	
+void setWidth (int i) {
+	width = i;
+}
+
+void setHeight (int i) {
+	height = i;
+}
+
+}
