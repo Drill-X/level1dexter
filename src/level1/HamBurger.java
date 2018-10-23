0ef5d3acab3e17c0793946fd20b3cb779f980111
Dexter H <league@WTS-IM-6.attlocal.net>
2018-10-22 18:27:25 -0700

    Hamburgerness

---
+++ src/level1/HamBurger.java
@@ -0,0 +1,17 @@
+package level1;
+
+public class HamBurger {
+private int Pounds;
+private Boolean isHasBacon;
+
+ public HamBurger(int Pounds, boolean isHasBacon) {
+	this.isHasBacon = isHasBacon;
+	this.Pounds = Pounds;
+}
+
+
+public void setBacon (boolean b) {
+	this.isHasBacon = b;
+}
+
+} 
